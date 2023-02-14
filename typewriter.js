35bd124d35d63c7423ec49f2d8a87ab627cb22b3
Anastasia <z.anastasia11@gmail.com>
2023-02-14 14:09:31 -0800

    add typeriter function

---
+++ typewriter.js
@@ -0,0 +1,26 @@
+const sentence = "hello there from lighthouse labs";
+
+const typewriter = () => {
+  let delay = 0;
+
+  for (const char of sentence) {
+    // avoid automatically adding an extra "newline character" at the end each time
+
+    setTimeout(() => {
+      // print the char here
+      process.stdout.write(char);
+
+
+    }, delay); // <= 1s delay to make it noticeable. Can dial it down later.
+
+    delay += 50;
+
+
+  }
+  setTimeout(() => {
+    console.log("\n");
+  }, delay);
+
+};
+
+typewriter(sentence);
